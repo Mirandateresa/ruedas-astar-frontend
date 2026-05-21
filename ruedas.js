666b607a4117fc3bcd6a69cf44aaa56c62648b19
Mirandateresa <154276040+Mirandateresa@users.noreply.github.com>
2026-05-21 00:28:19 -0600

    Update ruedas.js

--- ruedas.js
+++ ruedas.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://ruedas-astar-api.onrender.com/api';
 
 async function cargarPrecios() {
     console.log('Cargando precios de ruedas...');
